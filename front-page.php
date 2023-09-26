3ba685f12838f4352b00c6e9016f30dbe6266bac
allysonbelo <allysonbelo@gmail.com>
2023-09-26 19:30:18 -0300

    Add: Section 2 finalizada

--- front-page.php
+++ front-page.php
@@ -1,7 +1,10 @@
 <?php get_header() ?>
 
 <main>
-    <?php get_template_part('/parts/hero'); ?>
+    <?php
+    get_template_part('/parts/hero');
+    get_template_part('/parts/section2');
+    ?>
 </main>
 
 <?php get_footer() ?>
---
+++ parts/section2.php
@@ -0,0 +1,18 @@
+<section class="container section__2">
+    <div class="wrapper">
+        <div class="section__2__content">
+            <div class="section__2__left">
+                <h2>As a veterinarian and lover of animals.</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam enim odit vero laboriosam nulla veritatis voluptatem dolores?</p>
+                <div class="section__2__buttons">
+                    <a href="#" class="button__primary--yellow">Our Service</a>
+                </div>
+            </div>
+            <div class="section__2__right">
+                <a href="#">
+                    <img src="<?php echo get_theme_file_uri('assets/img/thumbnail-video.webp') ?>" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
