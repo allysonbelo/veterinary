2d5ae5cd703a13a2c5353036004f6b61f8ea89a4
allysonbelo <allysonbelo@gmail.com>
2023-09-26 18:59:00 -0300

    Add: Section Hero finalizada

---
+++ parts/hero.php
@@ -0,0 +1,17 @@
+<section class="container hero">
+    <div class="wrapper">
+        <div class="hero__content">
+            <div class="hero__left">
+                <h1>First I wanted to be a veterinarian</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam enim odit vero laboriosam nulla veritatis voluptatem dolores?</p>
+                <div class="hero__buttons">
+                    <a href="#" class="button__primary--blue ">Contact Us</a>
+                    <a href="#" class="button__secondary ">Our Service</a>
+                </div>
+            </div>
+            <div class="hero__right">
+                <img src="<?php echo get_theme_file_uri('/assets/img/cao-bulldog-frances.webp') ?>" alt="">
+            </div>
+        </div>
+    </div>
+</section>
