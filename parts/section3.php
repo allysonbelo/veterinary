af9a7b85e382c05d114b0ce0a1598f7994085b46
allysonbelo <allysonbelo@gmail.com>
2023-09-29 18:51:47 -0300

    Style: Ajustes nos tamanhos dos textos considerados grandes

---
+++ parts/section3.php
@@ -0,0 +1,18 @@
+<section class="container section__3">
+    <div class="wrapper">
+        <div class="section__3__content">
+            <div class="section__3__left">
+                <h2>As a veterinarian and lover of animals.</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam enim odit vero laboriosam nulla veritatis voluptatem dolores?</p>
+                <div class="section__3__buttons">
+                    <a href="#" class="button__primary--yellow">Our Service</a>
+                </div>
+            </div>
+            <div class="section__3__right">
+                <a href="#">
+                    <img src="<?php echo get_theme_file_uri('assets/img/thumbnail-video.webp') ?>" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
